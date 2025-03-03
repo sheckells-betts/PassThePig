ccadffae671b8570b39efcf2cafaf5128995fcfe
sheckells-betts <Lsheckellsbetts@brooksschool.org>
2025-03-03 13:36:56 +0000

    First commit

---
+++ PassThePigs.java
@@ -0,0 +1,5 @@
+public PassThePigs{
+    public static void main String(args[]){
+        
+    }
+}
---
+++ Player.java
@@ -0,0 +1,22 @@
+public Player{
+
+private String name;
+private String strategy;
+
+public Player(String name, String strategy){
+
+}
+
+public boolean wantsToRoll(int myScore, int handScore, ArrayList<Integer> otherScores, int winningScore){
+    
+}
+public String getName(){
+    return name;
+
+}
+public String getStrategy(){
+    return strategy;
+
+}
+
+}
