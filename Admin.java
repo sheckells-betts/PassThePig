eba1ff669fdc4e4ee6544bb4995efa59cc814c5e
sheckells-betts <Lsheckellsbetts@brooksschool.org>
2025-03-06 13:28:11 +0000

    added roll types and probabilty

--- Admin.java
+++ Admin.java
@@ -1,5 +1,7 @@
 public class Admin {
 private ArrayList <Integer> scores = new ArrayList<Integer>();
+private String[] rolls = {"Dot","No Dot", "Razorback", "Trotter", "Snouter", "Leaning Jowler"}; 
+private double[] probs = {34.90, 30.20, 22.4, 8.80, 3, 0.70};
 
 
 }
